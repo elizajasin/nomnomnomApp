370cbb900223248cd595dafc40372105138534a8
elizajasin <eliza_jasin@hotmail.com>
2016-11-25 01:04:26 +0700

    add cereal recipes layout

---
+++ app/src/main/java/net/jasin/eliza/nomnomnom/DarkChocolateGranola.java
@@ -0,0 +1,13 @@
+package net.jasin.eliza.nomnomnom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DarkChocolateGranola extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dark_chocolate_granola);
+    }
+}
