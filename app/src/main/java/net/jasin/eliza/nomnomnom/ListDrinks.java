77cd6f3d0eef9b8d1acf4a9683290192da80f972
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 22:23:41 +0700

    listDrink done

--- app/src/main/java/net/jasin/eliza/nomnomnom/ListDrinks.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/ListDrinks.java
@@ -1,7 +1,9 @@
 package net.jasin.eliza.nomnomnom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListDrinks extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ListDrinks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_drinks);
+    }
+
+    public void peppermint(View v){
+        Intent intent = new Intent(ListDrinks.this, PeppermintCappucino.class);
+        startActivity(intent);
+    }
+
+    public void anti(View v){
+        Intent intent = new Intent(ListDrinks.this, Antioxidant.class);
+        startActivity(intent);
+    }
+
+    public void banana(View v){
+        Intent intent = new Intent(ListDrinks.this, BananaOatmeal.class);
+        startActivity(intent);
     }
 }
