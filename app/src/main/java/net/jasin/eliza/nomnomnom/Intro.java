8c613bdcf8df1cd350353f66e699a4da1d2a5294
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 20:32:27 +0700

    add klikList

--- app/src/main/java/net/jasin/eliza/nomnomnom/Intro.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/Intro.java
@@ -32,6 +32,41 @@ public class Intro extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void klikDrink(View v){
+        Intent intent = new Intent(Intro.this, ListDrinks.class);
+        startActivity(intent);
+    }
+
+    public void klikSoup(View v){
+        Intent intent = new Intent(Intro.this, ListSoups.class);
+        startActivity(intent);
+    }
+
+    public void klikCookie(View v){
+        Intent intent = new Intent(Intro.this, ListCookies.class);
+        startActivity(intent);
+    }
+
+    public void klikSalad(View v){
+        Intent intent = new Intent(Intro.this, ListSalads.class);
+        startActivity(intent);
+    }
+
+    public void klikBread(View v){
+        Intent intent = new Intent(Intro.this, ListBreads.class);
+        startActivity(intent);
+    }
+
+    public void klikIce(View v){
+        Intent intent = new Intent(Intro.this, ListIces.class);
+        startActivity(intent);
+    }
+
+    public void klikCake(View v){
+        Intent intent = new Intent(Intro.this, ListCakes.class);
+        startActivity(intent);
+    }
+
     public void logout(View v){
         Intent intent = new Intent(Intro.this, Login.class);
         startActivity(intent);
