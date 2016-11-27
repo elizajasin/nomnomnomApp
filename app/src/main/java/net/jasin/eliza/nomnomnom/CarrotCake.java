b06ebd49cd32cf30b5b5ec8bf53002980135c818
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 22:10:01 +0700

    listBread done

---
+++ app/src/main/java/net/jasin/eliza/nomnomnom/CarrotCake.java
@@ -0,0 +1,13 @@
+package net.jasin.eliza.nomnomnom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarrotCake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrot_cake);
+    }
+}
--- app/src/main/java/net/jasin/eliza/nomnomnom/ListBreads.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/ListBreads.java
@@ -1,7 +1,9 @@
 package net.jasin.eliza.nomnomnom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListBreads extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ListBreads extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_breads);
+    }
+
+    public void apple(View v){
+        Intent intent = new Intent(ListBreads.this, Apple.class);
+        startActivity(intent);
+    }
+
+    public void carrotcake(View v){
+        Intent intent = new Intent(ListBreads.this, CarrotCake.class);
+        startActivity(intent);
+    }
+
+    public void chocchip(View v){
+        Intent intent = new Intent(ListBreads.this, ChocoChipPumpkin.class);
+        startActivity(intent);
     }
 }
