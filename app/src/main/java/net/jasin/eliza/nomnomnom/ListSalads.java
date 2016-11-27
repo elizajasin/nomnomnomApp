94e2a93a88a76a897db67a314fd132705a68d62a
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 22:33:18 +0700

    listSalad done

--- app/src/main/java/net/jasin/eliza/nomnomnom/ListSalads.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/ListSalads.java
@@ -1,7 +1,9 @@
 package net.jasin.eliza.nomnomnom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListSalads extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ListSalads extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_salads);
+    }
+
+    public void water(View v){
+        Intent intent = new Intent(ListSalads.this, WatermelonBenedict.class);
+        startActivity(intent);
+    }
+
+    public void crumble(View v){
+        Intent intent = new Intent(ListSalads.this, Crumble.class);
+        startActivity(intent);
+    }
+
+    public void usa(View v){
+        Intent intent = new Intent(ListSalads.this, UsaBreakfastParfaits.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/net/jasin/eliza/nomnomnom/UsaBreakfastParfaits.java
@@ -0,0 +1,13 @@
+package net.jasin.eliza.nomnomnom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsaBreakfastParfaits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usa_breakfast_parfaits);
+    }
+}
