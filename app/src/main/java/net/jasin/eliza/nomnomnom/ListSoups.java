3060353ea9690c88f9104bff9995f455b900e65b
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 22:36:36 +0700

    listSoup done

--- app/src/main/java/net/jasin/eliza/nomnomnom/ListSoups.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/ListSoups.java
@@ -1,7 +1,9 @@
 package net.jasin.eliza.nomnomnom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListSoups extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ListSoups extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_soups);
+    }
+
+    public void savory(View v){
+        Intent intent = new Intent(ListSoups.this, Savory.class);
+        startActivity(intent);
+    }
+
+    public void japan(View v){
+        Intent intent = new Intent(ListSoups.this, JapanMiso.class);
+        startActivity(intent);
+    }
+
+    public void thai(View v){
+        Intent intent = new Intent(ListSoups.this, ThaiKaoTom.class);
+        startActivity(intent);
     }
 }
