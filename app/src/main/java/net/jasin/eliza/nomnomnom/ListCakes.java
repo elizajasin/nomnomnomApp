8fe8592adbe7a5531ef8e7b8e974ede137256aa7
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 22:14:42 +0700

    listCake done

--- app/src/main/java/net/jasin/eliza/nomnomnom/ListCakes.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/ListCakes.java
@@ -1,7 +1,9 @@
 package net.jasin.eliza.nomnomnom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListCakes extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ListCakes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_cakes);
+    }
+
+    public void oatmeal(View v){
+        Intent intent = new Intent(ListCakes.this, Oatmeal.class);
+        startActivity(intent);
+    }
+
+    public void blueberry(View v){
+        Intent intent = new Intent(ListCakes.this, Blueberry.class);
+        startActivity(intent);
+    }
+
+    public void coffee(View v){
+        Intent intent = new Intent(ListCakes.this, CoffeeFlop.class);
+        startActivity(intent);
     }
 }
