f690bceaf7813214d9c4be3ee52e0fadb50579e5
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 22:28:53 +0700

    listIce done

--- app/src/main/java/net/jasin/eliza/nomnomnom/ListIces.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/ListIces.java
@@ -1,7 +1,9 @@
 package net.jasin.eliza.nomnomnom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListIces extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ListIces extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_ices);
+    }
+
+    public void vanilla(View v){
+        Intent intent = new Intent(ListIces.this, Vanilla.class);
+        startActivity(intent);
+    }
+
+    public void chocereal(View v){
+        Intent intent = new Intent(ListIces.this, ChocholateChipCereal.class);
+        startActivity(intent);
+    }
+
+    public void pebble(View v){
+        Intent intent = new Intent(ListIces.this, PebblesCereal.class);
+        startActivity(intent);
     }
 }
