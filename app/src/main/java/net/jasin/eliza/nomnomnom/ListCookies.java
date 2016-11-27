524e21f7c237832a1fe6fdf726f9a65a98a2ac43
elizajasin <eliza_jasin@hotmail.com>
2016-11-27 22:20:17 +0700

    listCookie done

--- app/src/main/java/net/jasin/eliza/nomnomnom/ListCookies.java
+++ app/src/main/java/net/jasin/eliza/nomnomnom/ListCookies.java
@@ -1,7 +1,9 @@
 package net.jasin.eliza.nomnomnom;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ListCookies extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class ListCookies extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_cookies);
+    }
+
+    public void darkcho(View v){
+        Intent intent = new Intent(ListCookies.this, DarkChocolateGranola.class);
+        startActivity(intent);
+    }
+
+    public void wafer(View v){
+        Intent intent = new Intent(ListCookies.this, VanillaWafer.class);
+        startActivity(intent);
+    }
+
+    public void nobake(View v){
+        Intent intent = new Intent(ListCookies.this, NoBakeChoco.class);
+        startActivity(intent);
     }
 }
